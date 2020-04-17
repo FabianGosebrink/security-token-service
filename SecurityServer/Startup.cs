c8a5ae591d4b20cb12a43abd6c1e2cfb12e6e5ab
damienbod <damien_bod@hotmail.com>
2020-04-18 00:40:51 +0200

    fixing frame ancestors

--- SecurityServer/Startup.cs
+++ SecurityServer/Startup.cs
@@ -198,7 +198,12 @@ namespace StsServerIdentity
                 .StyleSources(s => s.Self())
                 .StyleSources(s => s.UnsafeInline())
                 .FontSources(s => s.Self())
-                .FrameAncestors(s => s.CustomSources("https://localhost:4200", "https://localhost:4201", "https://localhost:4202", "https://localhost:4203", "https://localhost:4204"))
+                .FrameAncestors(s => s.CustomSources("https://localhost:4200", 
+                    "https://localhost:4201", 
+                    "https://localhost:4202", 
+                    "https://localhost:4203", 
+                    "https://localhost:4204", 
+                    "https://offeringsolutions-sts.azurewebsites.net"))
                 .ImageSources(imageSrc => imageSrc.Self())
                 .ImageSources(imageSrc => imageSrc.CustomSources("data:"))
                 .ScriptSources(s => s.Self())
