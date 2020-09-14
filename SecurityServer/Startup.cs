17c706d782b96c3f0ce5b496211ddfa1c4308405
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-09-14 14:56:14 +0200

    allowing any origin

--- SecurityServer/Startup.cs
+++ SecurityServer/Startup.cs
@@ -71,17 +71,9 @@ namespace StsServerIdentity
                     builder =>
                     {
                         builder
-                            .AllowCredentials()
-                            .WithOrigins(
-                                "https://localhost:4200", 
-                                "https://localhost:4201", 
-                                "https://localhost:4202", 
-                                "https://localhost:4203",
-                                "https://localhost:4204",
-                                "https://proud-island-014203c10.azurestaticapps.net")
-                            .SetIsOriginAllowedToAllowWildcardSubdomains()
                             .AllowAnyHeader()
-                            .AllowAnyMethod();
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin();
                     });
             });
 
